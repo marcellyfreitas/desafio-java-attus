8ec99d304de865e58f2c005bdae1adb68f83bc9b
Marcelly <marcelly.moraes@sga.pucminas.br>
2026-06-10 18:47:14 -0300

    feat(frontend): criar projeto react com vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Gerenciador de Tarefas</h1>
+    </div>
+  );
+}
+
+export default App;
